e371e8d604b73f93c7b960730ecb57b3922b7122
pz <zielinski.przemek@gmail.com>
2020-07-13 11:26:56 +0200

    Added coord_grid function.

--- utils/mpl_utils.py
+++ utils/mpl_utils.py
@@ -6,6 +6,10 @@ Created on Tue 5 May 2020
 @author: Przemyslaw Zielinski
 """
 
+from matplotlib import rcParams
+from matplotlib.gridspec import GridSpec
+
+
 def set_figsize(textwidth_fraction=1.0, height_to_width_ratio=0.5):
     """
     Set aesthetic figure dimensions to avoid scaling in latex.
@@ -29,10 +33,31 @@ def set_figsize(textwidth_fraction=1.0, height_to_width_ratio=0.5):
     fig_dim: tuple
     Dimensions of figure in inches
     """
-    from matplotlib import rcParams
-
     document_width = rcParams["figure.figsize"][0]
     fig_width = textwidth_fraction * document_width
     fig_height = fig_width * height_to_width_ratio
 
     return (fig_width, fig_height)
+
+def coord_grid(fig, darray, xylim=[-1.1, 1.1], var='x'):
+    gs = GridSpec(3, 3, figure=fig, hspace=0.05, wspace=0.05)
+
+    for n, coord in enumerate(darray.T[:-1]):
+        for m in range(n+1):
+            ax = fig.add_subplot(gs[n, m])
+    #         ax.set_title(f"{n}, {m-1}")
+            ax.scatter(darray[:,m-1], coord, s=0.5)
+            ax.set_xlim(xylim)
+            ax.set_ylim(xylim)
+            ax.set_aspect('equal')
+            if n == 2:
+                ax.set_xlabel(f'${var}_{(m-1) % 4}$')
+    #             ax.set_xticks([-.25, 0.0, +.25])
+            else:
+                ax.set_xticklabels([])
+            if m == 0:
+                ax.set_ylabel(f'${var}_{n}$', rotation=0)
+    #             ax.set_yticks([-.25, 0.0, +.25])
+            else:
+                ax.set_yticklabels([])
+    return gs
