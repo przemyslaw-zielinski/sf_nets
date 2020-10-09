22d6636460d7d332932674d28594f8bcf806104e
przemekZet <zielinski.przemek@gmail.com>
2020-10-09 19:10:27 +0200

    Small updates

--- sf_nets/models/models.py
+++ sf_nets/models/models.py
@@ -30,3 +30,19 @@ class MahalanobisAutoencoder(SimpleAutoencoder):
             x_rec_covi = torch.pinverse(torch.as_tensor(ln_covs), rcond=1e-10)
 
         return self.mah_loss(x, x_rec, (x_covi + x_rec_covi))
+
+class SemiMahalanobisAutoencoder(SimpleAutoencoder):
+
+    def __init__(self, input_features, latent_features, hidden_features=[]):
+
+        super().__init__(input_features, latent_features, hidden_features=[])
+        self.mah_loss = MahalanobisLoss()
+
+    def set_system(self, system):
+        self.system = system
+
+    def loss(self, batch):
+        x, x_covi = batch
+        x_rec, _ = self(x)
+
+        return self.mah_loss(x, x_rec, x_covi)
