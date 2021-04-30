4c9a74cd55ca087e68d3e40a24de9267ce49f523
przemekZet <zielinski.przemek@gmail.com>
2021-04-30 11:07:28 +0200

    Updated figure for OHM.

--- scripts/cresc2d_path_data.py
+++ scripts/cresc2d_path_data.py
@@ -23,14 +23,14 @@ from utils.io_utils import io_path, get_script_name
 
 # settings
 ds_name = 'Cresc2'
-io_path = io_path(ds_name)
+iop = io_path(ds_name)
 script_name = get_script_name()
 
 # matplotlib settings
 plt.style.use("utils/manuscript.mplstyle")
 cdata, cslow, cfast = 'C0', 'C1', 'C2'  # colors
 
-dataset = getattr(datasets, ds_name)(io_path.dataroot)
+dataset = getattr(datasets, ds_name)(iop.dataroot)
 times, path = torch.load(dataset.raw / 'path.pt')
 
 fig, axs = plt.subplots(ncols=2,
@@ -44,7 +44,15 @@ axs[0].set_ylabel(r"$x^2$", rotation=0, labelpad=-5)
 axs[1].scatter(*dataset.data.T, c=cdata, label="data point")
 axs[1].scatter(*dataset.slow_proj.T, c=cslow, label="projection")
 axs[1].set_title("Train data")
-axs[1].legend()
+
+# settings
+ds_name = 'Ohm2'
+iop = io_path(ds_name)
+dataset = getattr(datasets, ds_name)(iop.dataroot)
+axs[1].scatter(*dataset.data.T, c=cdata, alpha=0.3, zorder=0)
+axs[1].scatter(*dataset.slow_proj.T, c=cslow, alpha=0.3, zorder=0)
+
+axs[1].legend(loc=3)
 
 for ax in axs:
     ax.set_xlabel(r"$x^1$", labelpad=-5)
@@ -52,5 +60,5 @@ for ax in axs:
     ax.set_xticks([-1, 1])
     ax.set_yticks([-1, 1])
 
-plt.savefig(io_path.figs / f"{script_name}.pdf")
+plt.savefig(iop.figs / f"{script_name}.pdf")
 plt.close()
