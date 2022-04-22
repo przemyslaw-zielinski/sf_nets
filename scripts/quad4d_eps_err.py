4d63df3fecf631d602a3dad405fa9fec4cbb9a16
Przemyslaw Zielinski <zielinski.przemek@gmail.com>
2022-04-22 14:31:04 +0200

    Perform experiments on quad4 with varying eps

--- scripts/quad4d_eps_err.py
+++ scripts/quad4d_eps_err.py
@@ -22,7 +22,9 @@ from sf_nets.utils.mpl_utils import scale_figsize
 from sf_nets.utils.io_utils import io_path, get_script_name
 
 ds_base_name = 'Quad4'
-eps_variants = ['0_001', '0_01', '0_1']
+eps_variants = ['0_001', '0_0031', '0_01', '0_031', '0_1', '0_31', '1_0']
+model_ids = ['mse_elu_3'] + [f'mse_elu_3_r{n}' for n in range(1, 10)]
+print(model_ids)
 
 script_name = get_script_name()
 base_path = io_path(ds_base_name)
@@ -34,6 +36,7 @@ cdata, cslow, cfast = 'C0', 'C1', 'C2'  # colors
 model_id = "mse_elu_3"
 ds_names = [f'{ds_base_name}_{eps}' for eps in eps_variants]
 
+avgs, stds = [], []
 fig, ax = plt.subplots(figsize=scale_figsize(height=.9)) #figsize=scale_figsize(width=4/3))
 for n, (ds_name, eps) in enumerate(zip(ds_names, eps_variants)):
     path = io_path(ds_name)
@@ -53,38 +56,56 @@ for n, (ds_name, eps) in enumerate(zip(ds_names, eps_variants)):
     dat_t.requires_grad_(True)
     g = torch.eye(1).repeat(len(dat_t),1,1).T
 
-    model_data = torch.load(path.models / f'{model_id}.pt')
-    model_arch = model_data['info']['architecture']
-    model_args = model_data['info']['arguments']
-    state_dict = model_data['best']['model_dict']
+    avg_per_eps = []
+    std_per_eps = []
+    for model_id in model_ids:
+        model_data = torch.load(path.models / f'{model_id}.pt')
+        model_arch = model_data['info']['architecture']
+        model_args = model_data['info']['arguments']
+        state_dict = model_data['best']['model_dict']
 
-    model = getattr(models, model_arch)(**model_args)
-    model.load_state_dict(state_dict)
-    model.eval()
+        model = getattr(models, model_arch)(**model_args)
+        model.load_state_dict(state_dict)
+        model.eval()
 
-    grads_norm = []
-    for gi in g:
-        v = model.encoder(dat_t)
-        v.backward(gi.T)
-        grad = dat_t.grad.clone()
-        grads_norm.append(grad / torch.linalg.norm(grad, dim=1, keepdim=True))
-        dat_t.grad.zero_()
-    grads_norm = torch.stack(grads_norm, dim=2)
+        grads_norm = []
+        for gi in g:
+            v = model.encoder(dat_t)
+            v.backward(gi.T)
+            grad = dat_t.grad.clone()
+            grads_norm.append(grad / torch.linalg.norm(grad, dim=1, keepdim=True))
+            dat_t.grad.zero_()
+        grads_norm = torch.stack(grads_norm, dim=2)
 
-    A = torch.vstack([f_evecs.T, grads_norm.T]).T
-    AT = torch.transpose(A, 1, 2)
-    AAT = torch.matmul(AT, A)
-    diff = torch.linalg.norm(AAT - np.eye(4), dim=(1,2))
-    diff = diff.numpy()
+        A = torch.vstack([f_evecs.T, grads_norm.T]).T
+        AT = torch.transpose(A, 1, 2)
+        AAT = torch.matmul(AT, A)
+        diff = torch.linalg.norm(AAT - np.eye(4), dim=(1,2))
+        diff = diff.numpy()
+        avg_per_eps.append(diff.mean())
+        std_per_eps.append(diff.std())
 
-    ax.boxplot(
-        diff,
-        positions=[n],
-        sym='',
-        labels=[f'{float(eps.replace("_","."))}']
-    )
+    avgs.append(np.mean(avg_per_eps))
+    stds.append(np.mean(std_per_eps))
+avgs = np.array(avgs)
+stds = np.array(stds)
 
-ax.set_xlabel('Epsilon')
+    # ax.boxplot(
+    #     diff,
+    #     positions=[n],
+    #     sym='',
+    #     labels=[f'{float(eps.replace("_","."))}']
+    # )
+eps_float = [float(f'{eps.replace("_",".")}') for eps in eps_variants]
+print(eps_float)
+
+ax.fill_between(eps_float, avgs + stds, avgs - stds, color=cdata, alpha=0.2)
+ax.plot(eps_float, avgs, color=cslow)
+
+ax.set_xscale('log')
+ax.set_xlim([0.001, 1.0])
+ax.set_xticks([0.001, 0.01, 0.1, 1.0])
+ax.set_xlabel(r'$\epsilon$')
 ax.set_ylabel('Error')
 
 plt.tight_layout()
